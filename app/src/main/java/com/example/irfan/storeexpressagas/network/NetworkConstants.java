cd8bb02d8e1e60f64c8b952d772a45730efe0d8f
irfan <asa_khann@hotmail.com>
2019-02-21 16:45:24 +0500

    n

--- app/src/main/java/com/example/irfan/storeexpressagas/network/NetworkConstants.java
+++ app/src/main/java/com/example/irfan/storeexpressagas/network/NetworkConstants.java
@@ -2,7 +2,7 @@ package com.example.irfan.storeexpressagas.network;
 
 public class NetworkConstants {
 
-    public static String BASE_URL="http://www.insidedemo.com/";
+    public static String BASE_URL="http://192.168.100.2:9091/";
     public final static  long CONNECTION_TIMEOUT = 25;
     public final static int DATA_SUCCESS=1;
     public final static int DATA_ERROR=0;
